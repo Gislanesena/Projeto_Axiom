4c12a2ab099f0f30950a68c7acea0424fedd3d86
Gislane <gislane.sena@icloud.com>
2026-03-10 23:03:14 -0300

    home-page

--- SiteUni/script.js
+++ SiteUni/script.js
@@ -3,6 +3,9 @@ const elemBotãoEsquerdo = document.querySelector(".btn-prev");
 const elemBotãoDireita = document.querySelector(".btn-next");
 const elemsImagem = document.querySelectorAll(".slides img");
 
+const primeiroClone = elemsImagem[0].cloneNode(true);
+elemSlides.appendChild(primeiroClone);
+
 let index = 0;
 
 elemBotãoEsquerdo.addEventListener('click', () =>{
@@ -13,21 +16,64 @@ elemBotãoEsquerdo.addEventListener('click', () =>{
 })
 
 elemBotãoDireita.addEventListener('click', () =>{
-    incrementarIndex
+    incrementarIndex();
     atualizarCarrossel();
  //   console.log(index);
 });
 
+// const incrementarIndex = () => {
+//     index++;
+//     if (index > elemsImagem.length -1) index = 0
+// }
+
 const incrementarIndex = () => {
     index++;
-    if (index > elemsImagem.length -1) index = 0
 }
+
+// const atualizarCarrossel = () => {
+//     elemSlides.style.transform = `translateX(-${index * 100}%)`;
+// }
 
 const atualizarCarrossel = () => {
+
+    elemSlides.style.transition = "transform 0.5s ease";
     elemSlides.style.transform = `translateX(-${index * 100}%)`;
+
+    if(index === elemsImagem.length){
+        setTimeout(() => {
+            elemSlides.style.transition = "none";
+            index = 0;
+            elemSlides.style.transform = `translateX(0%)`;
+        }, 500);
+    }
+
 }
 
-setInterval(() =>{
+const proximoSlide = () => {
     incrementarIndex();
     atualizarCarrossel();
-}, 4000);
+}
+
+let intervalo = setInterval(proximoSlide, 3000);
+
+elemSlides.addEventListener("mouseenter", () => {
+    clearInterval(intervalo);
+});
+
+elemSlides.addEventListener("mouseleave", () => {
+    intervalo = setInterval(proximoSlide, 4000);
+});
+
+const carrossel = document.querySelector(".carrossel");
+
+carrossel.addEventListener("mousemove", (e) => {
+
+    const rect = carrossel.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    carrossel.style.setProperty("--x", x + "px");
+    carrossel.style.setProperty("--y", y + "px");
+
+});
